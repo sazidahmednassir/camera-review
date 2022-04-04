563f598957d50ba4892d5b5b1f9960b03214f15d
sazidahmednassir <sazidahmednassir@gmail.com>
2022-04-03 22:33:18 -0800

    home page first section done  commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>heelo from home</h1>
+        <div className="container mt-4">
+        <div class="row">
+  <div class="col-sm-6">
+    <div class="card border-0">
+      <div class="card-body ">
+        <h5 class="card-title fs-1 mt-5">DSLR cameras are the perfect way to capture memories that will last a lifetime.</h5>
+        <p class="card-text fs-5 mt-3">DSLR cameras are great for photography enthusiasts who want to take their hobby to the next level. They offer manual controls that give you more flexibility and control over your photos, and they produce better.</p>
+       
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card border-0">
+      <div class="card-body">
+      <img src="https://m.media-amazon.com/images/I/81pz06ySqrL._AC_SX569_.jpg" class="card-img img-fluid" alt="..."></img>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
         </div>
     );
 };
