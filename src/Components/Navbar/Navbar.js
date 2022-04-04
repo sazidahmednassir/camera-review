dcb8407a5c0ae6f0e4c024d8d171504321026cff
sazidahmednassir <sazidahmednassir@gmail.com>
2022-04-03 20:42:49 -0800

    navbar inital  commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+            
+        </div>
+    );
+};
+
+export default Navbar;
